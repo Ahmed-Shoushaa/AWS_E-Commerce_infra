57e1f12838133131fc84ec251549290bd54f6f55
Ahmed Adel Shousha <117172376+Ahmed-Shoushaa@users.noreply.github.com>
2023-09-26 09:26:30 +0300

    change naming convention and add vars

--- network.tf
+++ network.tf
@@ -6,46 +6,46 @@ resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.production.id
 }
 
-resource "aws_subnet" "Public-subnet-az1" {
+resource "aws_subnet" "public_subnet_az1" {
   vpc_id     = aws_vpc.production.id
-  cidr_block = "10.0.1.0/24"
+  cidr_block = var.public_subnet_az1_cidr
   availability_zone = data.aws_availability_zones.az.names[0]
 }
-resource "aws_subnet" "mobile-api-az1" {
+resource "aws_subnet" "mobile_api_az1" {
   vpc_id     = aws_vpc.production.id
-  cidr_block = "10.0.1.0/24"
+  cidr_block = var.mobile_api_az1_cidr
   availability_zone = data.aws_availability_zones.az.names[0]
 }
-resource "aws_subnet" "infra-az1" {
+resource "aws_subnet" "infra_az1" {
   vpc_id     = aws_vpc.production.id
-  cidr_block = "10.0.1.0/24"
+  cidr_block = var.infra_az1_cidr
   availability_zone = data.aws_availability_zones.az.names[0]
 }
 
-resource "aws_subnet" "Public-subnet-az2" {
+resource "aws_subnet" "public_subnet_az2" {
   vpc_id     = aws_vpc.production.id
-  cidr_block = "10.0.1.0/24"
+  cidr_block = var.public_subnet_az2_cidr
   availability_zone = data.aws_availability_zones.az.names[1]
 }
-resource "aws_subnet" "mobile-api-az2" {
+resource "aws_subnet" "mobile_api_az2" {
   vpc_id     = aws_vpc.production.id
-  cidr_block = "10.0.1.0/24"
+  cidr_block = var.mobile_api_az2_cidr
   availability_zone = data.aws_availability_zones.az.names[1]
 }
-resource "aws_subnet" "infra-az2" {
+resource "aws_subnet" "infra_az2" {
   vpc_id     = aws_vpc.production.id
-  cidr_block = "10.0.1.0/24"
+  cidr_block = var.infra_az2_cidr
   availability_zone = data.aws_availability_zones.az.names[1]
 }
 
-resource "aws_nat_gateway" "nat-gw-az1" {
+resource "aws_nat_gateway" "nat_gw_az1" {
   allocation_id = aws_eip.eip1.id
-  subnet_id     = aws_subnet.Public-subnet-az1.id
+  subnet_id     = aws_subnet.public_subnet_az1.id
   depends_on = [aws_internet_gateway.igw]
 }
 
-resource "aws_nat_gateway" "nat-gw-az2" {
+resource "aws_nat_gateway" "nat_gw_az2" {
   allocation_id = aws_eip.eip2.id
-  subnet_id     = aws_subnet.Public-subnet-az2.id
+  subnet_id     = aws_subnet.public_subnet_az2.id
   depends_on = [aws_internet_gateway.igw]
 }
--- vars.tf
+++ vars.tf
@@ -1,4 +1,29 @@
 variable "vpc_production_cidr" {
     type = string
-    default = "10.0.0.0/16"
+    default = ""
+}
+
+variable "public_subnet_az1_cidr" {
+    type = string
+    default = ""
+}
+variable "mobile_api_az1_cidr" {
+    type = string
+    default = ""
+}
+variable "infra_az1_cidr" {
+    type = string
+    default = ""
+}
+variable "public_subnet_az2_cidr" {
+    type = string
+    default = ""
+}
+variable "mobile_api_az2_cidr" {
+    type = string
+    default = ""
+}
+variable "infra_az2_cidr" {
+    type = string
+    default = ""
 }

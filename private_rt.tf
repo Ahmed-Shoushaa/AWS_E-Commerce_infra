57d1ad13c190e1e06b89ea871cccf2b35fd6a68b
Ahmed Adel Shousha <117172376+Ahmed-Shoushaa@users.noreply.github.com>
2023-09-26 12:45:39 +0000

    .

--- private_rt.tf
+++ private_rt.tf
@@ -1,11 +1,18 @@
+# Need adjustment
 resource "aws_route_table" "private_rt" {
   vpc_id = aws_vpc.production.id
 }
 
-resource "aws_route" "private_route" {
+resource "aws_route" "private_route1" {
   route_table_id         = aws_route_table.private_rt.id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_nat_gateway.nat_gw_az1
+}
+
+resource "aws_route" "private_route2" {
+  route_table_id         = aws_route_table.private_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_nat_gateway.nat_gw_az2
 }
 
 resource "aws_route_table_association" "mobile1subnet_rt-assoc" {
